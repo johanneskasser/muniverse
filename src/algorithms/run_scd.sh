6f132eefe1e6551bfddaf05e8d7b87469c10d993
pranavm19 <pranav.mamidanna@gmail.com>
2025-03-28 20:36:31 +0000

    Modify run_scd.sh to consider docker and singularity workflows

--- src/algorithms/run_scd.sh
+++ src/algorithms/run_scd.sh
@@ -2,23 +2,37 @@
 set -e
 
 # Usage:
-# ./run_scd.sh path/to/run_scd.py path/to/input.npy path/to/output_dir
+# ./run_scd.sh docker|singularity path/to/run_scd.py path/to/input.npy path/to/output_dir
 
-SCRIPT_PATH=$1
-DATA_PATH=$2
-OUTPUT_DIR=$3
+ENGINE=$1
+SCRIPT_PATH=$2
+DATA_PATH=$3
+OUTPUT_DIR=$4
 
-if [ -z "$SCRIPT_PATH" ] || [ -z "$DATA_PATH" ] || [ -z "$OUTPUT_DIR" ]; then
-  echo "Usage: ./run_scd.sh run_scd.py input.npy output_dir"
+if [ "$ENGINE" == "docker" ]; then
+  echo "[INFO] Running with Docker"
+  docker run --rm -it \
+    --gpus all \
+    -v $(realpath $SCRIPT_PATH):/opt/scd/run_scd.py \
+    -v $(realpath $DATA_PATH):/data/input.npy \
+    -v $(realpath $OUTPUT_DIR):/output/ \
+    pranavm19/muniverse-test:scd \
+    bash -c "source /opt/mambaforge/etc/profile.d/conda.sh && \
+             conda activate decomposition && \
+             cd /opt/scd/ && \
+             python run_scd.py /data/input.npy /output/"
+elif [ "$ENGINE" == "singularity" ]; then
+  echo "[INFO] Running with Singularity"
+  singularity run --nv --cleanenv \
+    -B $(realpath $SCRIPT_PATH):/opt/scd/run_scd.py \
+    -B $(realpath $DATA_PATH):/data/input.npy \
+    -B $(realpath $OUTPUT_DIR):/output/ \
+    muniverse-test_scd.sif \
+    bash -c "source /opt/mambaforge/etc/profile.d/conda.sh && \
+             conda activate decomposition && \
+             cd /opt/scd/ && \
+             python run_scd.py /data/input.npy /output/"
+else
+  echo "ERROR: Unknown engine '$ENGINE'. Use 'docker' or 'singularity'."
   exit 1
 fi
-
-docker run -it \
-  -v $(realpath $SCRIPT_PATH):/opt/scd/run_scd.py \
-  -v $(realpath $DATA_PATH):/data/input.npy \
-  -v $(realpath $OUTPUT_DIR):/output/ \
-  pranavm19/muniverse-test:scd \
-  bash -c "source /opt/mambaforge/etc/profile.d/conda.sh && \
-           conda activate decomposition && \
-           cd /opt/scd/ && \
-           python run_scd.py /data/input.npy /output/"
