023e75329bbdb9758f16da783b4f4755ead019e1
klotz-t <thomas.klotz@mechbau.uni-stuttgart.de>
2025-04-18 13:26:48 +0200

    Internally data is handled as edf signal. However there is a new routine for conversion into numpy.

--- src/data_preparation/data2bids.py
+++ src/data_preparation/data2bids.py
@@ -6,25 +6,24 @@ from edfio import *
 
 class emg_bids_generator:
 
-    def __init__(self, subject, task, datatype, root, session = -1, id_options=2):
+    def __init__(self, subject, task, datatype, root, session = -1, n_digits=2):
       
         # Check if the function arguments are valid
-        if type(subject) is not int or subject > 10**id_options-1:
+        if type(subject) is not int or subject > 10**n_digits-1:
             raise ValueError('invlaid subject ID')
         
-        if type(session) is not int or session > 10**id_options-1:
+        if type(session) is not int or session > 10**n_digits-1:
             raise ValueError('invlaid session ID')
         
         if datatype not in ['emg']:
             raise ValueError('datatype must be emg')
 
         # Process name and session input
+        sub_name = 'sub' + '-' + str(subject).zfill(n_digits)
         if session < 0:
-            sub_name = 'sub' + '-' + str(subject).zfill(id_options)
             datapath = root + '/' + sub_name + '/' + datatype + '/'
         else:
-            sub_name = 'sub' + '-' + str(subject).zfill(id_options)
-            ses_name = 'ses' + '-' + str(session).zfill(id_options)
+            ses_name = 'ses' + '-' + str(session).zfill(n_digits)
             datapath = root + '/' + sub_name + '/' + ses_name + '/' + datatype + '/'
         
         # Store essential information for BIDS compatible folder structure in a dictonary
@@ -33,8 +32,8 @@ class emg_bids_generator:
         self.task = 'task-' + task
         self.subject_id = sub_name
         self.datatype = datatype
-        self.data = np.empty
-        self.fsamp = float()
+        self.data = Edf([EdfSignal(np.zeros(1), sampling_frequency=1)])
+        self.fsamp = 1
         self.channels = pd.DataFrame(columns=['name', 'type', 'unit'])
         self.electrodes = pd.DataFrame(columns=['name','x','y','z', 'coordinate_system'])
         self.subject = pd.DataFrame(columns=['name', 'age', 'sex', 'hand', 'weight', 'height'])
@@ -43,17 +42,16 @@ class emg_bids_generator:
         self.coord_sidecar = {'EMGCoordinateSystem': [], 'EMGCoordinateUnits': []}
         self.dataset_sidecar = {'Name': [], 'BIDSversion': 'unpublished'}
         self.subject_sidecar = {'name': []} 
-
-        # Generate an empty set of folders for hosting your BIDS dataset
-        if not os.path.exists(datapath):
-            os.makedirs(datapath)  
+  
 
     def write(self):
         """
         Save dataset in BIDS format
 
         """
-
+        # Generate an empty set of folders for your BIDS dataset
+        if not os.path.exists(self.datapath):
+            os.makedirs(self.datapath)
         # write *_channels.tsv
         name = self.datapath + self.subject_id + '_' + self.task + '_' + 'channels' 
         self.channels.to_csv(name + '.tsv', sep='\t', index=False, header=True)
@@ -79,27 +77,9 @@ class emg_bids_generator:
         name = self.root + '/' + 'dataset.json'
         with open(name, 'w') as f:
             json.dump(self.dataset_sidecar, f) 
-
-        # Convert data into edf format 
-        seconds = np.ceil(self.data.shape[0]/self.fsamp)
-        # Add zeros to the signal such that the total length is in full seconds
-        signal = np.zeros([int(seconds*self.fsamp), self.data.shape[1]])
-        signal[0:self.data.shape[0],:] = self.data
-
-        edf = Edf([EdfSignal(signal[:,0], sampling_frequency=self.fsamp, 
-                             label=self.channels.loc[0, 'name'],
-                             physical_dimension=self.channels.loc[0, 'unit'])])
-
-        for i in np.arange(1,signal.shape[1]):
-            new_signal = EdfSignal(signal[:,i], 
-                                   sampling_frequency=self.fsamp, 
-                                   label=self.channels.loc[i, 'name'],
-                                   physical_dimension=self.channels.loc[i, 'unit'])
-            edf.append_signals(new_signal)
-
-        # Get a BIDS compatible path and filename 
+        # write edf file 
         name = self.datapath + self.subject_id + '_' + self.task + '_' + self.datatype
-        edf.write(name + '.edf')  
+        self.data.write(name + '.edf')  
 
         return()
 
@@ -141,23 +121,21 @@ class emg_bids_generator:
             with open(name, 'r') as f:
                 self.dataset_sidecar = json.load(f) 
         # read edf file
-        name = self.datapath + self.subject_id + '_' + self.task + '_' + self.datatype + '.edf'        
-        if os.path.isfile(name):
-            edf = read_edf(name)
-            self.fsamp = edf.signals[0].sampling_frequency
-            self.data = np.zeros((edf.signals[0].data.shape[0], edf.num_signals))
-            for i in np.arange(edf.num_signals):
-                self.data[:,i] = edf.signals[i].data
-  
+        name = self.datapath + self.subject_id + '_' + self.task + '_' + self.datatype + '.edf'
+        self.data = read_edf(name)
+        self.fsamp = self.data.signals[0].sampling_frequency        
+              
         return()  
                       
 
-    def add_channel_metadata(self, channel_metadata):
+    def add_channel_metadata(self, new_metadata):
         """
         Add channel metadata
 
         Args:
-            channel_metadata (dict): Channel metadata with essential keys
+            new_metadata (dict or pd.core.frame.DataFrame): Metadata
+
+            List of essential keys (ordering matters):
                 - name (string)
                 - type (string)
                 - units (string)
@@ -165,19 +143,27 @@ class emg_bids_generator:
         """
     
         # If an electrode already exists overwrite existing metadata
-        df_new = pd.DataFrame(data=channel_metadata)
-        frames = [self.channels, df_new]
+        if type(new_metadata) == dict:
+            df_new = pd.DataFrame(data=new_metadata)
+            frames = [self.channels, df_new]
+        elif type(new_metadata) == pd.core.frame.DataFramect:
+            frames = [self.channels, new_metadata]
+        else:
+            raise ValueError('input has incorrect datatype')
+        
         self.channels = pd.concat(frames)
         self.channels = self.channels.drop_duplicates(subset='name', keep='last')
 
         return()
 
-    def add_electrode_metadata(self, el_metadata):
+    def add_electrode_metadata(self, new_metadata):
         """
         Add electrode metadata
 
         Args:
-            el_metadata (dict): electrode metadata with essential keys
+            new_metadata (dict or pd.core.frame.DataFrame): Metadata
+
+            List of essential keys (ordering matters): 
                 - name (string)
                 - x (float)
                 - y (float)
@@ -187,24 +173,32 @@ class emg_bids_generator:
         """
 
         # If an electrode already exists overwrite existing metadata
-        df_new = pd.DataFrame(data=el_metadata)
-        frames = [self.electrodes, df_new]
+        if type(new_metadata) == dict:
+            df_new = pd.DataFrame(data=new_metadata)
+            frames = [self.electrodes, df_new]
+        elif type(new_metadata) == pd.core.frame.DataFramect:
+            frames = [self.electrodes, new_metadata]
+        else:
+            raise ValueError('input has incorrect datatype')
+        
         self.electrodes = pd.concat(frames)
         self.electrodes = self.subject.drop_duplicates(subset=['name', 'coordinate_system'], keep='last')
 
-        # If the z coordinate exist set it at the right position
+        # If the z coordinate exist make sure the ordering is correct
         if 'z' in self.electrodes.columns:
             col = self.electrodes.pop('z')
             self.electrodes.insert(3, 'z', col)    
 
         return()
 
-    def set_emg_sidecar(self, emg_metadata):
+    def add_emg_sidecar_metadata(self, new_metadata):
         """
         Update metadata of the emg sidecar file 
 
         Args:
-            emg_metadata (dict): metadata with essential keys
+            new_metadata (dict or path): metadata 
+
+            List of essential keys: 
                 - EMGPlacementScheme (str)
                 - EMGReference (str)
                 - SamplingFrequency (float)
@@ -214,27 +208,38 @@ class emg_bids_generator:
 
         """
 
-        self.emg_sidecar.update(emg_metadata)
+        if type(new_metadata) == dict:
+            self.emg_sidecar.update(new_metadata)
+        elif type(new_metadata) == str and os.path.isfile(new_metadata):
+            with open(new_metadata, 'r') as f:
+                tmp = json.load(f)
+            self.emg_sidecar.update(tmp)    
 
         return()
 
-    def set_coordsystem_sidecar(self, coordsystem_metadata):
+    def add_coordsystem_sidecar_metadata(self, new_metadata):
         """
         Add metadata that should be stored in coordsystem.json 
 
         Args:
-            bids_path (dict): filename and filepath information
-            coordsystem_metadata (dict): metadata with essential keys
+            new_metadata (dict or path): metadata 
+
+            List of essential keys:
                 - EMGCoordinateSystem (str)
                 - EMGCoordinateUnits (str)
 
         """
 
-        self.coord_sidecar.update(coordsystem_metadata)
+        if type(new_metadata) == dict:
+            self.coord_sidecar.update(new_metadata)
+        elif type(new_metadata) == str and os.path.isfile(new_metadata):
+            with open(new_metadata, 'r') as f:
+                tmp = json.load(f)
+            self.coord_sidecar.update(tmp)  
 
         return()
 
-    def add_subject(self, subject_metadata):
+    def add_subject_metadata(self, subject_metadata):
         """
         Add new subject 
 
@@ -256,7 +261,7 @@ class emg_bids_generator:
 
         return()
 
-    def set_participant_sidecar_temp(self,data_type):
+    def add_participant_sidecar_metadata_from_template(self,data_type):
         """
         Add metadata for a a BIDS compatible participants.json file from a predined template
 
@@ -289,7 +294,7 @@ class emg_bids_generator:
         
         return()
 
-    def set_dataset_sidecar(self, metadata):
+    def add_dataset_sidecar_metadata(self, metadata):
         """
         Add metadata to dataset_sidecar 
 
@@ -309,9 +314,9 @@ class emg_bids_generator:
 
         return()
     
-    def set_raw_data(self, data, fsamp):
+    def set_raw_data(self, mydata, fsamp):
         """
-        Add raw data
+        Add raw data and convert it into edf format
 
         Args:
             data (np.ndarry): emg_data (n_samples x n_channels)
@@ -319,10 +324,39 @@ class emg_bids_generator:
 
         """
 
-        self.data = data
+        # Add zeros to the signal such that the total length is in full seconds 
+        seconds = np.ceil(mydata.shape[0]/fsamp)
+        signal = np.zeros([int(seconds*fsamp), mydata.shape[1]])
+        signal[0:mydata.shape[0],:] = mydata
+
+        # Initalize
+        edf = Edf([EdfSignal(signal[:,0], sampling_frequency=fsamp)])
+
+        for i in np.arange(1,signal.shape[1]):
+            new_signal = EdfSignal(signal[:,i], sampling_frequency=self.fsamp)
+            edf.append_signals(new_signal)
+
+        self.data = edf
         self.fsamp = fsamp
 
         return()
+    
+    def edf_to_numpy(self, idx):
+        """
+        Output data of selcetd channels as numpy array
+
+        Args:
+            idx (ndarray): Indices of the channels to be stored
+
+        """
+
+        data_out = np.zeros((self.data.signals[idx[0]].data.shape[0], self.data.num_signals))
+        for i in np.arange(len(idx)):
+            data_out[:,i] = self.data.signals[idx[i]].data
+
+        return(data_out)    
+
+
 
 
 
