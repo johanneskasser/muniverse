844178fe34ef697166fa42356e26a3a0a0aab6d0
pranavm19 <pranav.mamidanna@gmail.com>
2025-06-09 15:25:35 +0100

    bug fix: circular import from src/__init__

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-MUniverse: A modular framework for motor unit decomposition algorithm benchmarking.
-"""
-
-from . import algorithms, data_generation, evaluation, utils
-
-__version__ = "0.1.0"
